b47e847091debd72dc7267c7eb7970269b57426e
Vovaman <bd_postbox1@mail.ru>
2021-11-02 18:05:50 +0300

    #18 is in progress

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from ldap_db import conn
 
 from app.api import tags
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    conn.bind()
+
+@app.on_event("shutdown")
+async def shutdown():
+    conn.unbind()
+
 
 app.include_router(tags.router)
