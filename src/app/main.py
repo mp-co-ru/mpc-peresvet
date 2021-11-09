baed5587c9e9cb0fa6e4eed868df221047c10e75
Vovaman <bd_postbox1@mail.ru>
2021-11-09 13:34:57 +0300

    #20 realized

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from app.api import tags
+from app.logger.PrsLogger import PrsLogger
 
-app = FastAPI()
+def create_app() -> FastAPI:
+    app = FastAPI(title='Peresvet', debug=False)
+    logger = PrsLogger.make_logger()
+    app.logger = logger
 
+    return app
+
+app = create_app()
 app.include_router(tags.router)
