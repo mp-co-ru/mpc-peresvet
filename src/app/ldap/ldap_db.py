55e6c85273ff3dcde4173de715970dd2765afcc4
Vovaman <bd_postbox1@mail.ru>
2021-11-12 19:44:17 +0300

    #19 in progress

--- src/app/ldap/ldap_db.py
+++ src/app/ldap/ldap_db.py
@@ -1,8 +1,19 @@
 import os
-from ldap3 import Server, Connection, SAFE_SYNC
-
-server_uri = os.getenv("LDAP_URI")
-server = Server(server_uri)
-uid = os.getenv("LDAP_USER")
-pwd = os.getenv("LDAP_PASSWORD")
-conn = Connection(server, uid, pwd, client_strategy=SAFE_SYNC, auto_bind=False)
+from ldap3 import SCHEMA, Server, Connection, SAFE_SYNC
+import app.svc.Services as svc
+class PrsLDAP:
+    
+    def __init__(self, host: str, port: str, uid: str, pwd: str):
+        svc.Services.logger.info("LDAP Server connecting...")
+        self._server = Server(host=host, port=port, get_info=SCHEMA)
+        svc.Services.logger.debug("uid: {}".format(uid))
+        self._write_conn = Connection(
+            self._server, user=uid, password=pwd, 
+            client_strategy=SAFE_SYNC, read_only=False,
+            pool_name="write_ldap", pool_size=1, auto_bind=True)
+        self._read_conn = Connection(
+            self._server, user=uid, password=pwd, 
+            client_strategy=SAFE_SYNC, read_only=True,
+            pool_name="read_ldap", pool_size=20, auto_bind=True)
+        svc.Services.logger.info("LDAP Server connected.")
+    
--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,17 @@
+import os
 from fastapi import FastAPI
 from app.api import tags
-from app.logger.PrsLogger import PrsLogger
+from app.svc.Services import Services
 
 def create_app() -> FastAPI:
+        
     app = FastAPI(title='Peresvet', debug=False)
-    logger = PrsLogger.make_logger()
-    app.logger = logger
-
     return app
 
 app = create_app()
-app.include_router(tags.router)
+
+Services.set_logger()
+app.logger = Services.logger
+Services.set_ldap()
+
+app.include_router(tags.router)   
