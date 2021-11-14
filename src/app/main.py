8c3874466cf33f468ef588721f8ca6b639cf8735
Vovaman <bd_postbox1@mail.ru>
2021-11-14 21:06:49 +0300

    #19 connection to ldap is added

--- src/app/main.py
+++ src/app/main.py
@@ -6,12 +6,11 @@ from app.svc.Services import Services
 def create_app() -> FastAPI:
         
     app = FastAPI(title='Peresvet', debug=False)
+    Services.set_logger()
+    Services.set_ldap()
+
     return app
 
 app = create_app()
-
-Services.set_logger()
-app.logger = Services.logger
-Services.set_ldap()
 
 app.include_router(tags.router)   
