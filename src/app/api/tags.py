1ef71d45eb316e8f95c21793c150679e472b957b
Vovaman <bd_postbox1@mail.ru>
2021-11-15 19:37:48 +0300

    #18 in progress

--- src/app/api/tags.py
+++ src/app/api/tags.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from fastapi import Request
 import json
+from app.ldap.model_node import pr_obj, pr_obj2
 
 router = APIRouter()
 
@@ -8,5 +9,12 @@ router = APIRouter()
 async def pong(req: Request):
     # some async operation could happen here
     # example: `notes = await get_all_notes()`
-    req.app.logger.info("Logger")
+    pr_obj()
     return {"tags": "yes!"}
+
+@router.get("/m")
+async def move(req: Request):
+    # some async operation could happen here
+    # example: `notes = await get_all_notes()`
+    pr_obj2()
+    return {"tags": "yes"}
---
+++ src/app/ldap/model_node.py
@@ -0,0 +1,53 @@
+from ldap3 import Connection, ObjectDef, Reader, Writer, Entry, BASE
+from uuid import uuid4
+from pydantic import BaseModel, create_model_from_typeddict
+from typing import List
+
+from app.svc.Services import Services
+
+'''
+class Prs_bm_ModelNode(BaseModel):
+    cn: str
+    description: List[str] = None
+    prsSystemNode: bool = False
+    prsEntityTypeCode: int = None
+    prsJsonConfigString: str = None
+    prsIndex: int = None
+    prsDefault: bool = None
+    prsActive: bool =  True
+    prsApp: List[str] = None
+'''
+
+class Prs_ldap_ModelNode(Entry):
+    objectClass: str = 'prsModelNode'
+        
+    @classmethod
+    def cast(cls, some_entry: Entry):
+        """Cast an A into a B."""
+        assert isinstance(some_entry, Entry)
+        some_entry.__class__ = cls
+        return some_entry
+
+
+def pr_obj():
+    conn = Services.ldap.get_read_conn()
+    o = ObjectDef('prsModelNode', conn)
+    r = Reader(conn, o, 'cn=tags,cn=prs')
+    r.search()
+    w = Writer.from_cursor(r)
+    n_e = w.new('cn=tag1,cn=tags,cn=prs')
+    n_e.uniqueIdentifier = str(uuid4())
+    n_e.entry_commit_changes()
+    Services.logger.info(o)
+
+def pr_obj2():    
+    conn = Services.ldap.get_write_conn()
+    o = ObjectDef('prsModelNode', conn)
+    r = Reader(conn, o, 'cn=tag1,cn=tags,cn=prs')
+    r.search()
+    w = Writer.from_cursor(r)
+    e = w[0]
+    e.entry_move('cn=schedules,cn=prs')
+    e.entry_commit_changes()
+    Services.logger.info(e)
+    
