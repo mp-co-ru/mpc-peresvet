3c048d805908afb3730f8c178287e860a9efb74f
Vovaman <bd_postbox1@mail.ru>
2021-11-16 19:41:23 +0300

    #18 in progress

--- src/app/models/models.py
+++ src/app/models/models.py
@@ -1,9 +1,33 @@
-from pydantic import create_model, BaseModel
+from pydantic import BaseModel
 from typing import List
 
-TagAttributes = None
+class PrsTagCreateAttrs(BaseModel):
+    cn: str = None
+    prsValueTypeCode: int = 0
+    prsSource: str = None
+    prsStore: str = None 
+    prsMeasureUnits: str = None
+    prsMaxDev: float = None
+    prsMaxLineDev: float = None
+    prsArchive: bool = True
+    prsCompress: bool = True 
+    prsValueScale: float = None
+    prsStep: bool = False
+    prsUpdate: bool = True
+    prsDefaultValue: str = None
+    description: List[str] = None
+    prsSystemNode: bool = None
+    prsEntityTypeCode: int = None
+    prsJsonConfigString: dict = None
+    prsIndex: int = None
+    prsDefault: bool = None
+    prsActive: bool = True
+    prsApp: List[str] = None
 class PrsTagCreate(BaseModel):
     parentId: str = None
     dataSourceId: str = None
-    dataStorageId: str = None
-    attributes: TagAttributes
+    attributes: PrsTagCreateAttrs
+        
+
+
+    
