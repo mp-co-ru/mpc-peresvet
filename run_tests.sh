3127cd400686ca2e2601f3cf46a4a1faf79ae864
Vovaman <bd_postbox1@mail.ru>
2021-12-08 17:27:14 +0300

    closes #37. new container with ldap server for testing is added

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 docker-compose -f docker-compose.yml -f docker-compose.test.yml stop
 docker container rm ldap_test
-docker-compose -f docker-compose.yml -f docker-compose.test.yml up -d --build
+docker-compose -f docker-compose.yml -f docker-compose.test.yml up -d
 docker-compose exec peresvet pytest --cov=./app . 
