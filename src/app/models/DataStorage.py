9e102c5bae485d0ae039af52d593ae5b8a731cea
Vovaman <bd_postbox1@mail.ru>
2021-12-10 18:13:18 +0300

    #44 in progress

--- src/app/models/DataStorage.py
+++ src/app/models/DataStorage.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, validator, Field, root_validator
 from typing import List, Optional, Union, Any
 import json
+from ldap3 import LEVEL, DEREF_SEARCH
 import validators
 from app.svc.Services import Services as svc
 from app.models.ModelNode import PrsModelNodeCreateAttrs, PrsModelNodeCreate, PrsModelNodeEntry
@@ -53,9 +54,45 @@ class PrsDataStorageEntry(PrsModelNodeEntry):
     objectClass: str = 'prsDataStorage'
     payload_class = PrsDataStorageCreate
     default_parent_dn: str = "cn=dataStorages,{}".format(svc.config["LDAP_BASE_NODE"])
+
+    def __init__(self, **kwargs):
+        super(PrsDataStorageEntry, self).__init__(**kwargs)
+
+        self.tags_cache = {}
+        self._read_tags()
     
+    def _read_tags(self):
+
+        result, _, response, _ = svc.ldap.get_read_conn.search(
+            search_base="cn=tags,{}".format(self.dn),
+            search_filter='(cn=*)', search_scope=LEVEL, 
+            dereference_aliases=DEREF_SEARCH, 
+            attributes=['entryUUID', 'prsDataStore'])
+        if not result:
+            svc.logger.info("Нет привязанных тэгов к хранилищу '{}'".format(self.data.attributes.cn))
+            return
+        
+        for item in response:
+            attrs = dict(item['attributes'])
+            self.tags_cache[attrs['entryUUID']] = attrs['prsDataStore']
+        
+        svc.logger.info("Тэги, привязанные к хранилищу `{}`, прочитаны.".format(self.data.attributes.cn))
+        
+
     def connect(self): pass
 
     def set_data(self, data: List[Any]): pass
 
     def get_data(self, Any):  pass
+
+    def _add_subnodes(self) -> None:
+        data = PrsModelNodeCreate()
+        data.parentId = self.id
+        data.attributes = PrsModelNodeCreateAttrs(cn='tags')
+        PrsModelNodeEntry(svc.ldap.get_write_conn(), data=data)
+
+        data.attributes.cn = 'alerts'
+        PrsModelNodeEntry(svc.ldap.get_write_conn(), data=data)
+    
+    def reg_tag(self, id: tag_id):
+        
