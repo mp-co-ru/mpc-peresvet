444dce468757a9a29c9a63a65b2c86223ea00c4e
Vovaman <bd_postbox1@mail.ru>
2022-08-13 13:53:12 +0300

    test corrected

--- src/app/models/Data.py
+++ src/app/models/Data.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel, Field
 from typing import List, Union
 
 class PrsDataItem(BaseModel):
-    x: Union[int, str] = Field(None, title="Метка времени", 
+    x: Union[int, str] = Field(None, title="Метка времени",
        description="Может быть либо целым числом, в этом случае это наносекунды")
-    y: Union[None, int, float, str, dict]
+    y: Union[None, float, int, dict, str]
     q: int = None
 
 class PrsTagData(BaseModel):
