99e412cb8f705b6d08cf86ba07dd25a6671e4c82
Vovaman <bd_postbox1@mail.ru>
2023-01-11 20:57:18 +0300

    #15 realized for int, float. there are no unit tests and docs

--- src/app/api/tags.py
+++ src/app/api/tags.py
@@ -6,7 +6,7 @@ import app.models.Tag as Tag
 router = APIRouter()
 
 @router.post("/", response_model=ModelNode.PrsResponseCreate, status_code=201)
-async def create(payload = Depends(Tag.PrsTagCreate)):
+async def create(payload : Tag.PrsTagCreate):
     tag = await main.app.create_tag(payload)
     id_ = tag.get_id()
     return {"id": id_}
